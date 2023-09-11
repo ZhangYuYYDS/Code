22e33c80ebbee65da66512c163ebbecb62225d16
zhangyu <1015371461@qq.com>
2023-09-11 19:59:46 +0800

    最长连续序列

---
+++ 力扣/128.最长连续序列.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var longestConsecutive = function (nums) {
+    if (nums.length == 0) return 0;
+
+    let len = nums.length;
+    let count = 1;
+    let result = 1;
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < len; i++) {
+        if (nums[i] == nums[i + 1]) continue;
+        if (nums[i] + 1 == nums[i + 1]) {
+            count++;
+        } else {
+            result = Math.max(result, count);
+            count = 1;
+        }
+    }
+    return result;
+};
